651f38676f128a8d9c207358695b43fa05abd573
CC2019 <458123902@qq.com>
2020-12-13 11:17:25 -0800

    create lambda function

---
+++ lambda_function.py
@@ -0,0 +1,33 @@
+import json
+import boto3
+
+print('Loading function')
+
+
+def lambda_handler(event, context):
+    
+    client = boto3.client('sqs')
+    response = client.receive_message(QueueUrl='https://sqs.us-east-1.amazonaws.com/026298698340/SearchQueue', 
+                                      MaxNumberOfMessages=5)
+    if 'Messages' in response.keys():
+        print('Number of Messages: {}'.format(len(response['Messages'])))
+        for i in range(len(response['Messages'])):
+            print('New Message')
+            body = response['Messages'][i]['Body']
+            
+            handle = response['Messages'][i]['ReceiptHandle']
+            deleteResponse = client.delete_message(QueueUrl='https://sqs.us-east-1.amazonaws.com/026298698340/SearchQueue',
+                                ReceiptHandle=handle)
+            
+            info = body.split('|')
+            cuisine = info[0]
+            phone = info[1]
+            number = info[2]
+            date = info[3]
+            time = info[4]
+            print(cuisine + ' ' + phone)
+    else:
+        print('Empty Queue')
+    print(response)
+    return 'Finished'  # Echo back the first key value
+    #raise Exception('Something went wrong')
