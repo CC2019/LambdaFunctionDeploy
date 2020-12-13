23ef69021d12cd5aadfcfa819af58a0047099c21
CC2019 <458123902@qq.com>
2020-12-13 11:56:50 -0800

    test-3

--- lambda_function.py
+++ lambda_function.py
@@ -7,5 +7,5 @@ print('Loading function')
 
 def lambda_handler(event, context):
     
-    return 'Hello World'  # Echo back the first key value
+    return 'Hello World - 1'  # Echo back the first key value
     #raise Exception('Something went wrong')
