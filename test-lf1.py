f415097178c7cfaa54044e653253c890da8662a3
CC2019 <458123902@qq.com>
2020-12-13 13:10:05 -0800

    test-4

---
+++ test-lf1.py
@@ -0,0 +1,11 @@
+import json
+
+import boto3
+
+print('Loading function')
+
+
+def lambda_handler(event, context):
+    
+    return 'Hello World - 2'  # Echo back the first key value
+    #raise Exception('Something went wrong')
